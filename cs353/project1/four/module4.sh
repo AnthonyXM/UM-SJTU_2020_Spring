08c5727da1a239e812dd639063aef4959d0d4573
AnthonyXM <anthony12590@126.com>
2020-04-12 19:59:29 +0800

    1

--- cs353/project1/four/module4.sh
+++ cs353/project1/four/module4.sh
@@ -5,11 +5,11 @@ sudo insmod module4.ko
 lsmod | head -2
 dmesg | tail -1
 echo "---- Show the content of module4 ----"
-cat /proc/module4
+cat /proc/module4_dir/module4
 echo "--------- Write to module4 ---------"
 echo "new module4 text" > /proc/module4
 echo "-- Show the new content of module4 --"
-cat /proc/module4
+cat /proc/module4_dir/module4
 echo "---------- Remove module 4 ----------"
 sudo rmmod module4.ko
 dmesg | tail -1
