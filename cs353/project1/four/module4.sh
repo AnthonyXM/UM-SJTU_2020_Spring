24a87052cceed055b83f3c0f70ad638a87d07f9d
AnthonyXM <anthony12590@126.com>
2020-04-12 20:00:19 +0800

    1

--- cs353/project1/four/module4.sh
+++ cs353/project1/four/module4.sh
@@ -7,7 +7,7 @@ dmesg | tail -1
 echo "---- Show the content of module4 ----"
 cat /proc/module4_dir/module4
 echo "--------- Write to module4 ---------"
-echo "new module4 text" > /proc/module4
+echo "new module4 text" > /proc/module4_dir/module4
 echo "-- Show the new content of module4 --"
 cat /proc/module4_dir/module4
 echo "---------- Remove module 4 ----------"
